6992b633fe8bae180d5b7f2f5a6fa8fd2bba5d08
Akihito KIRISAKI <endouryusuke@realglobe.jp>
2019-03-14 18:58:06 +0900

    atoi: power

--- atoi.fs
+++ atoi.fs
@@ -1,4 +1,16 @@
-: ctoi ( c -- n )
-  0x30 - dup 10 > if
+: ctoi ( c -- u )
+  0x30 - dup dup -1 < swap 9 > or if
     throw
     then ;
+
+: pow ( u1 u2 -- u )
+  1+ 0 do i .
+       i 0= if 1 else over * then
+    loop
+  .s throw
+;
+
+: atoi ( c-addr u -- u )
+  2dup 0 max 0 ?do dup c@ ctoi swap 1+ loop
+  \ 0 swap 0 do dup v + loop
+;

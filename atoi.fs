2810ecddc511d540d07cb6dcae3e45e4d650f674
Akihito KIRISAKI <endouryusuke@realglobe.jp>
2019-03-14 19:22:48 +0900

    atoi: Sum

--- atoi.fs
+++ atoi.fs
@@ -4,13 +4,17 @@
     then ;
 
 : pow ( u1 u2 -- u )
-  1+ 0 do i .
+  1+ 0 do
        i 0= if 1 else over * then
-    loop
-  .s throw
+    loop nip
+;
+
+: sum ( u1 u2 .. n -- u )
+  0 max 0 ?do dup + loop
+  .s cr throw
 ;
 
 : atoi ( c-addr u -- u )
-  2dup 0 max 0 ?do dup c@ ctoi swap 1+ loop
-  \ 0 swap 0 do dup v + loop
+  tuck 1- 0 swap do dup c@ ctoi 10 i pow * swap rot .s swap 1+ -1 +loop drop
+  .s throw
 ;
